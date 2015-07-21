b10207d119059ae51ae2556e2aeb04706a4fc596
KurinchiMalar <kurinchimalar.n@gmail.com>
2015-07-22 00:33:23 +0530

    Initial commit

---
+++ src/main/java/springmvc/HelloController.java
@@ -0,0 +1,11 @@
+package springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.
+/**
+ * Created by Elcot on 16-Jul-15.
+ */
+@Controller
+public class HelloController {
+    @RequestMapp
+}
---
+++ src/main/java/springmvc/SpringApplication.java
@@ -0,0 +1,16 @@
+package springmvc;
+
+import javafx.application.Application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Elcot on 16-Jul-15.
+ */
+@SpringBootApplication
+public class SpringApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(SpringApplication.class,args);
+    }
+
+}
